629ff5e429e0edb105aab0b65a1dbba1fa2ef9ba
Tommy Trujillo <TTrujillo5@my.gcu.edu>
2019-01-17 13:33:30 -0700

    Third Commit

---
+++ Calories/Calories/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gets user input
+            Console.WriteLine("How many cookies did you eat?");
+            string input = Console.ReadLine();
+            int cookies = Convert.ToInt32(input);
+
+            //Data types needed for calculations
+            int serving = 4;
+            int calories = 300;
+            int caloriesperserving = calories / serving;
+
+            //Calculations
+            int consumedcalories = cookies * caloriesperserving;
+            Console.WriteLine("You consumed " + consumedcalories + " total calories.");
+
+            Console.ReadKey();
+        }
+    }
+}
