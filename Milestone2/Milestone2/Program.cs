d20ebfc7d354dacb5563f0e11b65b6ae49b4252d
Tommy Trujillo <TTrujillo5@my.gcu.edu>
2019-03-09 19:25:56 -0700

    cst-117 milestone2 submission

---
+++ Milestone2/Milestone2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the name of the disc.");
+            String discname = Console.ReadLine();
+
+            Console.WriteLine("Enter the ID# for the disc.");
+            int idnum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the color of the disc.");
+            String discColor = Console.ReadLine();
+
+            Console.WriteLine("Enter the weight of the disc.");
+            int discWeight = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Milestone2Updated/Milestone2Updated/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone2Updated
+{
+    public class Disc
+    {
+        public string productid { get; set; }
+        public string productname { get; set; }
+        public double productprice { get; set; }
+        public int quantityonhand { get; set; }
+        public string productcolor { get; set; }
+        public string productweight { get; set; }
+        
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Disc newdisc = new Disc();
+            newdisc.productname = "Gladiator";
+            newdisc.productid = "1";
+            newdisc.productprice = 12.50;
+            newdisc.quantityonhand = 20;
+            newdisc.productcolor = "Pink";
+            newdisc.productweight = "175 grams";
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
