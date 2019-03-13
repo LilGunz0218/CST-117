d4e4fc5b2685755602750a7c9cab998bd19adcab
Tommy Trujillo <TTrujillo5@my.gcu.edu>
2019-03-12 23:37:29 -0700

    Assignment7 TurnIn, Stubs.cs for grading

---
+++ Assignment7/Assignment7/Stubs.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    class Stubs
+    {
+        // Tommy Trujillo III
+        // This is my own work.
+
+
+        //#1
+        public static void showSum(int numberone, int numbertwo)
+        {
+
+        }
+
+        // #2
+        public static double returnAverage(double numberone, double numbertwo, double numberthree, double numberfour, double numberfive)
+        {
+            
+        }
+
+        // #3
+        public static int returnRandomSum (int rand1, int rand2)
+        {
+           
+        }
+
+        // #4
+        public static Boolean divisible(int num1, int num2, int num3)
+        {
+           
+        }
+
+        // #5
+        public static void fewerChars(string string1, string string2)
+        {
+
+        }
+
+        // #6
+        public static double largerArray(double[] array1, double[] array2)
+        {
+
+        }
+
+        // #7
+        public static int generateArray(int[] array1)
+        {
+
+        }
+
+        // #8
+        public static Boolean checkValue(bool bool1, bool bool2)
+        {
+
+        }
+
+        // #9
+        public static double generateProduct(double num1, int num2)
+        {
+
+        }
+
+        // #10
+        public static double returnAverage(int[][] array1)
+        {
+
+        }
+
+    }
+}
