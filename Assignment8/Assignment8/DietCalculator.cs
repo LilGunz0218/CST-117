fd9f35258fbbe5d7c3c0a52879937be832a1a794
Tommy Trujillo <TTrujillo5@my.gcu.edu>
2019-03-13 13:28:53 -0700

    Assignment8 TurnIn See Program.cs for grading

---
+++ Assignment8/Assignment8/DietCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    class DietCalculator
+    {
+        public static double FatCalories (double fatgrams)
+            {
+            Console.WriteLine("Enter how many Fat Grams you consumed today.");
+
+            fatgrams = Convert.ToDouble(Console.ReadLine());
+
+            double fatCalories = fatgrams * 9;
+
+            Console.WriteLine("You consumed " + fatCalories + " fat calories!");
+            return fatCalories;
+            }
+
+        public static double CarbCalories (double carbgrams)
+        {
+            Console.WriteLine("Enter how many Carb Grams you consumed today.");
+
+            carbgrams = Convert.ToDouble(Console.ReadLine());
+
+            double carbCalories = carbgrams * 4;
+            Console.WriteLine("You consumed " + carbCalories + " carb calories!");
+            return carbCalories;
+        }
+    }
+}
---
+++ Assignment8/Assignment8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    class Program
+    {
+
+        //Tommy Trujillo III
+        //This is my own work.
+        public static double FatCalories(double fatgrams)
+        {
+
+            double fatCalories = fatgrams * 9;
+            Console.WriteLine("You consumed " + fatCalories + " Carb calories today.");
+            return fatCalories;
+        }
+
+        public static double CarbCalories(double carbgrams)
+        {
+            double carbCalories = carbgrams * 4;
+            Console.WriteLine("You consumed " + carbCalories + " Carb calories today.");
+            return carbCalories;
+        }
+
+        public static void Main(string[] args)
+        {
+
+                CarbCalories(48.0);
+                FatCalories(128.6);
+
+
+
+                Console.ReadKey();
+
+        }
+        
+    }
+}
